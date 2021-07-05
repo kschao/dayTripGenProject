2077c79327b9d302f46c2486a66ce29141128489
John Doe <dustin.koy@Dustins-MacBook-Pro.local>
2021-07-05 16:18:44 -0500

    test message

---
+++ dayTrip1.js
@@ -0,0 +1,26 @@
+"use strict"
+
+
+const places = ["San Francisco", "Emeryville", "Los Angeles", "Santa Cruz", "Napa Valley", "Sacramento"];
+const random = Math.floor(Math.random() * places.length);
+    console.log(random, places[random]);{
+
+    }
+
+const foods = ["Pizza", "Mexican Food", "Chinese Cusine", "Sushi", "Italian Food", "Mediterranean Food", "Wings", "Seafood"];
+const random2 = Math.floor(Math.random() * foods.length);
+    console.log(random, foods[random]);{
+
+    }
+
+const rides = ["Cab", "Bus", "Uber", "Lyft", "Personal Car", "Bike Rental"];
+const random3 = Math.floor(Math.random() * rides.length);
+    console.log(random, rides[random]);{
+
+    }
+  
+    if (confirm('Is this the destination you wish to confirm?')) {
+    console.log('Your trip has completed');
+      } else {
+      }
+      
