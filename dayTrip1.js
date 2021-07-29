8915de0dc37295b9a01e6b1b0dcb4b582620261f
kschao <koyschao@icloud.com>
2021-07-29 09:19:55 -0500

    fixed codes and now have entertainment, completed!

--- dayTrip1.js
+++ dayTrip1.js
@@ -1,26 +1,28 @@
 "use strict"
 
+prompt("Welcome to 'Day Trip Generator in California! Click OK to start");
 
-const places = ["San Francisco", "Emeryville", "Los Angeles", "Santa Cruz", "Napa Valley", "Sacramento"];
-const random = Math.floor(Math.random() * places.length);
-    console.log(random, places[random]);{
+let name= prompt ("What is your name?");
 
-    }
+let destination = ["San Francisco", "Emeryville", "Los Angeles", "Santa Cruz", "Napa Valley", "Sacramento"];
+let ran_destination = destination[Math.floor(Math.random()*restaurant.length)];
+prompt("You will be in the city of...",ran_destination);
 
-const foods = ["Pizza", "Mexican Food", "Chinese Cusine", "Sushi", "Italian Food", "Mediterranean Food", "Wings", "Seafood"];
-const random2 = Math.floor(Math.random() * foods.length);
-    console.log(random, foods[random]);{
 
-    }
+let restaurant = ["Pizza", "Mexican Food", "Chinese Buffet", "Sushi", "Italian", "Mediterranean", "Wings", "Seafood"];
+let rand_rest = restaurant[Math.floor(Math.random()*restaurant.length)];
+prompt("You will be dining in at...",rand_rest);
 
-const rides = ["Cab", "Bus", "Uber", "Lyft", "Personal Car", "Bike Rental"];
-const random3 = Math.floor(Math.random() * rides.length);
-    console.log(random, rides[random]);{
 
-    }
-  
-    if (confirm('Is this the destination you wish to confirm?')) {
-    console.log('Your trip has completed');
-      } else {
-      }
-      
+let transportation = ["Bus", "Taxi", "Uber", "Lyft", "Rental", "Bike"];
+let rand_trans = transportation[Math.floor(Math.random()*transportation.length)];
+prompt("You will take the...",rand_trans);
+
+let entertainment = ["Comedy Club", "Movies", "Arcade Center", "Beach", "Amusement Park", "Water Park"];
+let rand_enter = entertainment[Math.floor(Math.random()*transportation.length)];
+console.log("You will go...", rand_enter);
+
+let final = (name)+ "will begin this exciting trip in" + (ran_destination) + "Later that evening, you will be sitting down to have a delicious meal at" + (rand_rest) + "During the day, you will be utilizing a/an" + (rand_trans) + "to see all the beautiful sites. Lastly, you will make fun memories at" + (rand_enter) + "Hope you enjoy your trip. Click OK";
+prompt(final);
+alert(final);
+console.log(final);
